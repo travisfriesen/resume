d7687e08e1a5a78eac45800c88894b1fab9ccca0
travisfriesen <github@travisfriesen.ca>
2024-01-29 12:06:57 -0600

    resume to main

--- main.py
+++ main.py
@@ -1,42 +1,13 @@
-from pylatex import Document, Section, Subsection, Command
-from pylatex.utils import italic, NoEscape
-from pdflatex.pdflatex import PDFLaTeX
+import datetime
 
+import generateComponent
 
-def fill_document(doc):
-    """Add a section, a subsection and some text to the document.
+datetime = datetime.datetime.now().strftime('%m%d%y-%H%M%S')
+resumetitle = "ResumeGenerated" + datetime
+sources = generateComponent.generate_sources()
+data = generateComponent.generateFile()
+generateComponent.createFile(resumetitle)
+generateComponent.appendFile(resumetitle, sources)
+generateComponent.appendFile(resumetitle, data)
 
-    :param doc: the document
-    :type doc: :class:`pylatex.document.Document` instance
-    """
-    with doc.create(Section('A section')):
-        doc.append('Some regular text and some ')
-        doc.append(italic('italic text. '))
-
-        with doc.create(Subsection('A subsection')):
-            doc.append('Also some crazy characters: $&#{}')
-
-
-if __name__ == '__main__':
-    # Basic document
-    doc = Document('basic')
-    fill_document(doc)
-
-    doc.generate_pdf(clean_tex=True)
-    doc.generate_tex()
-
-    # Document with `\maketitle` command activated
-    doc = Document()
-
-    doc.preamble.append(Command('title', 'Awesome Title'))
-    doc.preamble.append(Command('author', 'Anonymous author'))
-    doc.preamble.append(Command('date', NoEscape(r'\today')))
-    doc.append(NoEscape(r'\maketitle'))
-
-    fill_document(doc)
-
-    doc.generate_pdf('basic_maketitle', clean_tex=False)
-
-    # Add stuff to the document
-    with doc.create(Section('A second section')):
-        doc.append('Some text.')
+generateComponent.make_pdf(resumetitle)
