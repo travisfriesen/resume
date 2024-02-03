16e5f0d79441d7ac54e328746a96ea77fe9840b0
travisfriesen <github@travisfriesen.ca>
2024-02-02 22:47:12 -0600

    generate header

--- generateComponent.py
+++ generateComponent.py
@@ -5,32 +5,61 @@ import jsonExtracter
 texdir = 'output/tex/'
 pdfdir = 'output/pdf/'
 
+
 def generate_sources():
-    with open ('resumecomponents/sources.tex', 'r') as file:
+    with open('resumecomponents/sources.tex', 'r') as file:
         sources = file.read().rstrip('\n')
 
     return sources
 
-def generate_header(config_filename):
 
+def generate_header(config_filename):
+    data = get_header_data(config_filename)
     num_elements = jsonExtracter.getNumElements(config_filename)
-    data1 = "\n\\begin{center}\n\\textbf{\Huge \scshape "
-    #for i in range(num_elements):
-        #print(i)
+    data1 = "\n%----------HEADING----------\n\\begin{center}\n\\textbf{\\Huge \\scshape " + data[1] + "} \\\\ \\vspace{1pt}\n"
+    for i in range(int(num_elements) - 1):
+        if i != int(num_elements) - 2:
+            if data[i*3 + 3] == 'text':
+                data1 = data1 + "\\small " + data[i*3 + 4] + " $|$\n "
+            elif data[i*3 + 3] == 'email' or data[i*3 + 3] == 'link':
+                data1 = data1 + "\\href{ " + data[i*3 + 5] + "}{\\underline{" + data[i*3 + 4] + "}} $|$\n "
+        else:
+            if i == int(num_elements) - 2:
+                if data[i*3 + 3] == 'text':
+                    data1 = data1 + "\\small " + data[i*3 + 4] + "\n "
+                elif data[i*3 + 3] == 'email' or data[i*3 + 3] == 'link':
+                    data1 = data1 + "\\href{ " + data[i*3 + 5] + "}{\\underline{" + data[i*3 + 4] + "}} \n "
+
+    data1 = data1 + "\\end{center}\n"
     return data1
 
 
-def generateFile():
+def combine_header_array(a, b):
+    a.extend(b)
+    return a
+
+
+def get_header_data(config_filename):
+    num_elements = jsonExtracter.getNumElements(config_filename)
+    data = []
+    for i in range(int(num_elements)):
+        data = combine_header_array(data, jsonExtracter.getHeaderElement(i))
+
+    return data
+
+
+def generate_file():
     with open('test.tex', 'r') as file:
         data = file.read().rstrip('\n')
 
     return data
 
 
-def createFile(resumetitle):
+def create_file(resumetitle):
     open(texdir + resumetitle + ".tex", "x")
 
-def appendFile(resumetitle, passed_data):
+
+def append_file(resumetitle, passed_data):
     with open(texdir + resumetitle + ".tex", "r") as file:
         source_file = file.read().rstrip('\n')
 
--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import generateComponent
 datetime = datetime.datetime.now().strftime('%m%d%y-%H%M%S')
 resumetitle = "ResumeGenerated" + datetime
 sources = generateComponent.generate_sources()
-data = generateComponent.generateFile()
-generateComponent.createFile(resumetitle)
-generateComponent.appendFile(resumetitle, sources)
-#bigdata = generateComponent.generate_header('config')
-#generateComponent.appendFile(resumetitle, bigdata)
-generateComponent.appendFile(resumetitle, data)
+data = generateComponent.generate_file()
+generateComponent.create_file(resumetitle)
+generateComponent.append_file(resumetitle, sources)
+bigdata = generateComponent.generate_header('config')
+generateComponent.append_file(resumetitle, bigdata)
+generateComponent.append_file(resumetitle, data)
 
 generateComponent.make_pdf(resumetitle)
